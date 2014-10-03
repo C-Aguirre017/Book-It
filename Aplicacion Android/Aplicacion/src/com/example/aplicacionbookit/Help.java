9561941879d6b43eb73e8da6a90d1d87820d0f76
Carlos Aguirre <c.aguirre017@gmailcom>
2014-10-03 18:00:06 -0400

    Aplicacion en rama principal

---
+++ Aplicacion Android/Aplicacion/src/com/example/aplicacionbookit/Help.java
@@ -0,0 +1,18 @@
+package com.example.aplicacionbookit;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Help extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_help);
+		
+		android.app.ActionBar bariita= getActionBar();
+		bariita.hide();
+	}
+}

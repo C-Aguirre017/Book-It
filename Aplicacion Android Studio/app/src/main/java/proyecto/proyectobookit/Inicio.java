aad588e6e46c14c8ccf4a3071ef653736c7f6dc0
Enrique Correa <quiquecorreav@gmail.com>
2015-02-18 17:33:14 -0300

    fb login

--- Aplicacion Android Studio/app/src/main/java/proyecto/proyectobookit/Inicio.java
+++ Aplicacion Android Studio/app/src/main/java/proyecto/proyectobookit/Inicio.java
@@ -1,28 +1,95 @@
 package proyecto.proyectobookit;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.facebook.FacebookException;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
 
-public class Inicio extends ActionBarActivity {
+import java.util.Arrays;
+
+
+public class Inicio extends FragmentActivity {
+
+    private LoginButton authButton;
+    private UiLifecycleHelper uiHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        uiHelper = new UiLifecycleHelper(this, statusCallback);
+        uiHelper.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_inicio);
+
+        authButton = (LoginButton) findViewById(R.id.authButton);
+        authButton.setReadPermissions(Arrays.asList("public_profile", "email"));
+        authButton.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
+            @Override
+            public void onUserInfoFetched(GraphUser user) {
+                if (user != null) {
+                    Intent app = new Intent(Inicio.this, Central.class);
+                    startActivity(app);
+                } else {
+                    Log.d("MainActivity", "You are not logged in.");
+                }
+            }
+        });
     }
 
-    public void BotonRegistrarse(View v){
-        Intent Registro = new Intent(this,Registro.class);
-        startActivity(Registro);
+    private Session.StatusCallback statusCallback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state, Exception exception) {
+            if (state.isOpened()) {
+                Log.d("MainActivity", "Facebook session opened2.");
+            } else if (state.isClosed()) {
+                Log.d("MainActivity", "Facebook session closed2.");
+            }
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        uiHelper.onResume();
     }
 
-    public void BotonLogin(View v ) {
-        Intent Login = new Intent(this,Central.class);
-        startActivity(Login);
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedState) {
+        super.onSaveInstanceState(savedState);
+        uiHelper.onSaveInstanceState(savedState);
     }
 }

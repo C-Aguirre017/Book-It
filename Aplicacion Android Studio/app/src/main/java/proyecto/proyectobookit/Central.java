be9c04634180ca392b7853d6b4f3b91d828cfad4
Enrique Correa <quiquecorreav@gmail.com>
2015-03-23 22:56:48 -0300

    no ocupar token de prueba

--- Aplicacion Android Studio/app/src/main/java/proyecto/proyectobookit/Central.java
+++ Aplicacion Android Studio/app/src/main/java/proyecto/proyectobookit/Central.java
@@ -70,7 +70,6 @@ public class Central extends Activity {
         setContentView(R.layout.activity_central);
 
         Mi_Usuario = Usuario.getUsuarioActual();
-        Mi_Usuario.setToken("LDskzPi1vfr31746VKG3");
 
         mTitle = mDrawerTitle = getTitle();
         navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
